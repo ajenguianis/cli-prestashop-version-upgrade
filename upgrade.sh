8b0bd69efd44f1fa1dd3caf060487fe7eda39bfd
Anis Ajengui <aajengui@evolutive-business.com>
2022-01-14 15:03:01 +0100

    first-commit

---
+++ upgrade.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+git clone "https://github.com/PrestaShop/autoupgrade.git" ./modules/autoupgrade
+cd ./modules/autoupgrade
+composer install --ignore-platform-reqs --no-interaction
+cd ../..
+php bin/console prestashop:module install autoupgrade
+php modules/autoupgrade/cli-upgrade.php  --dir=$1 --channel=major --action=upgradeNow
+php modules/autoupgrade/cli-upgrade.php  --dir=$1 --channel=major --action=upgradeDb
+php modules/autoupgrade/cli-upgrade.php  --dir=$1 --channel=major --action=upgradeModules
+php bin/console prestashop:module uninstall autoupgrade
+cd ./modules
+rm -rf autoupgrade
